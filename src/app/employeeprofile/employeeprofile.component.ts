86898b42cdb0b58e8685e0f29b543524f4b8d60a
chunkityip <alex0917lfo@hotmail.com>
2024-06-16 05:54:56 -0400

    Adding HTML , css and component at employeeprofile , employeeprofile component finished

--- src/app/employeeprofile/employeeprofile.component.ts
+++ src/app/employeeprofile/employeeprofile.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { EmployeeRegistationService } from '../employee-registation.service';
 
 @Component({
   selector: 'app-employeeprofile',
   templateUrl: './employeeprofile.component.html',
   styleUrls: ['./employeeprofile.component.css']
 })
-export class EmployeeprofileComponent {
 
+
+export class EmployeeprofileComponent implements OnInit {
+
+  employee : any;
+  name : string;
+  message : any;
+
+
+  //Constructor
+  constructor(private service : EmployeeRegistationService) {}
+
+  //ngOnInit lifecycle hock
+  ngOnInit() {
+      this.employee = this.service.employee;
+  }
 }
